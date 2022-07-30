a06d6b3f26d58fae2856dbebd222154f013eb3e9
chenkuan <812254315@qq.com>
2022-07-30 23:40:41 +0800

    init

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/Jacksmall/base_struct/dislogger
+
+go 1.16
+
+require (
+	github.com/jinzhu/copier v0.3.5 // indirect
+	github.com/lestrrat/go-file-rotatelogs v0.0.0-20180223000712-d3151e2a480f // indirect
+	github.com/lestrrat/go-strftime v0.0.0-20180220042222-ba3bf9c1d042 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+)
---
+++ zaplog/zaplog_test.go
@@ -0,0 +1,29 @@
+package zaplog
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+func TestZapLog(t *testing.T) {
+	data := &Options{
+		LogFileDir: "/Users/chenkuan/Code/gotest/dislogger/runtime/logs",
+		AppName:    "dislogger",
+		MaxSize:    1024,
+		MaxBackups: 7,
+		MaxAge:     90,
+		Config:     zap.Config{},
+	}
+	data.Development = true
+	initLogger(data)
+	for i := 0; i < 2; i++ {
+		time.Sleep(time.Second)
+		logger.Debug(fmt.Sprint("debug log ", i), zap.Int("line", 999))
+		logger.Info(fmt.Sprint("Info log ", i), zap.Any("level", "1231231231"))
+		logger.Warn(fmt.Sprint("warn log ", i), zap.String("level", `{"a":"4","b":"5"}`))
+		logger.Error(fmt.Sprint("err log ", i), zap.String("level", `{"a":"7","b":"8"}`))
+	}
+}
